f28bb1c56ac7b1ba97177aca6c6f6888286d079e
km2535 <km2535@naver.com>
2025-02-20 00:42:12 +0900

    [FEAT] 헤더 스타일 적용 및 공통 폰트 스타일 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        custom: ['"LineSeedFont"', "sans-serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
