7d73a136226c15c9c3b3ef8b54c9de2e0a5f3e8e
km2535 <km2535@naver.com>
2025-02-27 23:34:08 +0900

    [REFACTOR] 미사용 주석 제거 및 ESLINT 설정 변경

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
-import {dirname} from "path";
-import {fileURLToPath} from "url";
-import {FlatCompat} from "@eslint/eslintrc";
+import { FlatCompat } from "@eslint/eslintrc";
 import checkFilePlugin from "eslint-plugin-check-file";
 import unicornPlugin from "eslint-plugin-unicorn";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -29,9 +29,10 @@ const eslintConfig = [
                     ignore: [
                         "^globals\\.css$", // 전역 CSS 파일 예외 처리
                         "^favicon\\.ico$", // 파비콘 예외 처리
-                        "^layout\\.tsx$",  // Next.js 레이아웃 파일 예외 처리
-                        "^page\\.tsx$",    // Next.js 페이지 파일 예외 처리
-                        "^route\\.ts$",     // route.ts 파일 예외 처리 추가
+                        "^layout\\.tsx$", // Next.js 레이아웃 파일 예외 처리
+                        "^page\\.tsx$", // Next.js 페이지 파일 예외 처리
+                        "^route\\.ts$", // route.ts 파일 예외 처리 추가
+                        "^*.config\\.ts$", // config 설정 파일 예외 처리
                     ],
                 },
             ],
@@ -40,28 +41,34 @@ const eslintConfig = [
                 "error",
                 {
                     "*.tsx": "**/(components|app)/**", // .tsx는 components와 app 폴더 및 하위 경로 허용
-                    "*.ts": "**/(lib|util)/**",        // .ts는 lib와 util 폴더 및 하위 경로 허용
+                    "*.ts": "**/(lib|util)/**", // .ts는 lib와 util 폴더 및 하위 경로 허용
                 },
             ],
 
             // Check File : 추가할 네이밍 컨벤션 입력
             // ...
-
         },
     },
     {
-        ignores: [           // 무시 항목
+        ignores: [
+            // 무시 항목
             "node_modules/", // 기본적으로 무시되는 폴더
-            ".next/",        // Next.js 빌드 결과물
-            "dist/",         // 빌드 결과물
-            "public/",       // 정적 파일 폴더
-            "prisma/",       // Prisma 관련 스키마 폴더
-            "data/",         // 데이터 관련 폴더 (예: JSON 또는 CSV)
+            ".next/", // Next.js 빌드 결과물
+            "dist/", // 빌드 결과물
+            "public/", // 정적 파일 폴더
+            "prisma/", // Prisma 관련 스키마 폴더
+            "data/", // 데이터 관련 폴더 (예: JSON 또는 CSV)
         ],
     },
     // route.ts 파일에 대한 check-file 규칙 예외 추가 (즉, 해당 룰을 비활성화)
     {
         files: ["**/route.ts"],
+        rules: {
+            "check-file/folder-match-with-fex": "off",
+        },
+    },
+    {
+        files: ["*.config.ts"],
         rules: {
             "check-file/folder-match-with-fex": "off",
         },
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,23 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        custom: ['"LineSeedFont"', "sans-serif"],
-      },
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        primary: "#0d117a", // Added primary color
-        primaryThin: "#0045aa", // Added primary color
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                custom: ['"LineSeedFont"', "sans-serif"],
+            },
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+                primary: "#0d117a", // Added primary color
+                primaryThin: "#0045aa", // Added primary color
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
