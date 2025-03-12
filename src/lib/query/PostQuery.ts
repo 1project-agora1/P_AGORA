3d1c0a92484090bcfc976a24e773a610f21f4bc4
Tigris <kyrae604@naver.com>
2025-03-12 16:54:47 +0900

    [CHORE] 누락된 PR 변경 사항 : 페이지네이션 관련 쿼리

--- src/lib/query/PostQuery.ts
+++ src/lib/query/PostQuery.ts
@@ -3,23 +3,38 @@ import { generateRandomToken } from "@/util/RandomToken";
 import { PostCreateRequest } from "../request/PostRequest";
 
 export class PostQuery {
-    async findPreviewList(boardToken: string) {
-        const prisma = PrismaClientManager.getClient();
-        return prisma.post.findMany({
-            select: {
-                token: true, // 상세 조회 넘어가기 위한 토큰
-                title: true, // 미리 보기 리스트에서 보여줄 항목
-                views: true, // 미리 보기 리스트에서 보여줄 항목
-                likes: true, // 미리 보기 리스트에서 보여줄 항목
-            },
-            where: {
-                board_token: boardToken,
-            },
-            orderBy: {
-                createdAt: "desc", // 최신순 정렬
-            },
-        });
-    }
+  async findPreviewList(boardToken: string, page: number, pageSize: number) {
+    const prisma = PrismaClientManager.getClient();
+    const paginatedResults = await prisma.post.findMany({
+      select: {
+        token: true, // 상세 조회 넘어가기 위한 토큰
+        title: true, // 미리 보기 리스트에서 보여줄 항목
+        views: true, // 미리 보기 리스트에서 보여줄 항목
+        likes: true, // 미리 보기 리스트에서 보여줄 항목
+      },
+      where: {
+        board_token: boardToken,
+      },
+      orderBy: {
+        createdAt: "desc", // 최신순 정렬
+      },
+      skip: (page - 1) * pageSize, // 이전 페이지 데이터 스킵
+      take: pageSize, // 현재 페이지 데이터 가져오기
+    });
+
+    // 총 데이터 개수 계산
+    const totalCount = await prisma.post.count({
+      where: {
+        board_token: boardToken,
+      },
+    });
+    console.log(totalCount);
+    return {
+      data: paginatedResults,
+      totalCount, // 총 데이터 개수 반환
+      totalPages: Math.ceil(totalCount / pageSize), // 총 페이지 수 계산
+    };
+  }
 
     findPostDetail(postToken: string) {
         const prisma = PrismaClientManager.getClient();
