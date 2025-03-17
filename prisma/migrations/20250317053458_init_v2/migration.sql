1596cb3ffd640554db3040c248d429a288b91fca
Tigris <kyrae604@naver.com>
2025-03-17 15:06:33 +0900

    [CHORE] board 삭제 마이그레이션

---
+++ prisma/migrations/20250317053458_init_v2/migration.sql
@@ -0,0 +1,90 @@
+-- CreateTable
+CREATE TABLE `user` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `token` VARCHAR(191) NOT NULL,
+    `nickname` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(128) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `email_verified` BOOLEAN NOT NULL DEFAULT false,
+    `role` ENUM('ROLE_DEVELOPER', 'ROLE_ADMIN', 'ROLE_USER') NOT NULL DEFAULT 'ROLE_USER',
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `user_token_key`(`token`),
+    UNIQUE INDEX `user_nickname_key`(`nickname`),
+    UNIQUE INDEX `user_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `post` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `token` VARCHAR(191) NOT NULL,
+    `board_token` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `content` TEXT NOT NULL,
+    `user_token` VARCHAR(191) NOT NULL,
+    `views` INTEGER NOT NULL DEFAULT 0,
+    `likes` INTEGER NOT NULL DEFAULT 0,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `post_token_key`(`token`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `board` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `token` VARCHAR(255) NOT NULL,
+    `menu_token` VARCHAR(191) NOT NULL,
+    `board_name` VARCHAR(255) NOT NULL,
+    `description` TEXT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `board_token_key`(`token`),
+    UNIQUE INDEX `board_menu_token_key`(`menu_token`),
+    UNIQUE INDEX `board_board_name_key`(`board_name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `channel` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `token` VARCHAR(191) NOT NULL,
+    `menu_name` VARCHAR(191) NOT NULL,
+    `url` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `channel_token_key`(`token`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `channel_item` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `token` VARCHAR(191) NOT NULL,
+    `parent_menu_token` VARCHAR(191) NOT NULL,
+    `parent_submenu_token` VARCHAR(191) NULL,
+    `submenu_name` VARCHAR(191) NOT NULL,
+    `url` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `channel_item_token_key`(`token`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `post` ADD CONSTRAINT `post_user_token_fkey` FOREIGN KEY (`user_token`) REFERENCES `user`(`token`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `board` ADD CONSTRAINT `board_menu_token_fkey` FOREIGN KEY (`menu_token`) REFERENCES `channel_item`(`token`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `channel_item` ADD CONSTRAINT `channel_item_parent_menu_token_fkey` FOREIGN KEY (`parent_menu_token`) REFERENCES `channel`(`token`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `channel_item` ADD CONSTRAINT `channel_item_parent_submenu_token_fkey` FOREIGN KEY (`parent_submenu_token`) REFERENCES `channel_item`(`token`) ON DELETE SET NULL ON UPDATE CASCADE;
